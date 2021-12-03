24dd5693a348066acab901266bfff665af74c9aa
alirezamdv <ali.s3cret@gmail.com>
2021-12-03 13:36:06 +0100

    gitIgnore

---
+++ Bids.java
@@ -0,0 +1,46 @@
+public class Bids {
+    private String bidderUserID;
+    private String bidderTime;
+    private String itemID;
+    private String bidderAmount;
+
+    public String getBidderUserID() {
+        return bidderUserID;
+    }
+
+    public void setBidderUserID(String bidderUserID) {
+        this.bidderUserID = bidderUserID;
+    }
+
+    public String getBidderTime() {
+        return bidderTime;
+    }
+
+    public void setBidderTime(String bidderTime) {
+        this.bidderTime = bidderTime;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getBidderAmount() {
+        return bidderAmount;
+    }
+
+    public void setBidderAmount(String bidderAmount) {
+        this.bidderAmount = bidderAmount;
+    }
+
+    @Override
+    public String toString() {
+        return bidderUserID + '|' +
+                bidderTime + '|' +
+                itemID + '|' +
+                bidderAmount;
+    }
+}
