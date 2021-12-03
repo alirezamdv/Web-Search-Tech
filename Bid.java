2166af01c70838eb2d58e2d22cf582ef1e623399
alirezamdv <ali.s3cret@gmail.com>
2021-12-03 10:28:21 +0100

    gitignore

---
+++ Bid.java
@@ -0,0 +1,25 @@
+public class Bid {
+    private String BidderUserID;
+    private String Amount;
+
+
+
+    public String getBidderUserID() {
+        return BidderUserID;
+    }
+
+    public void setBidderUserID(String bidderUserID) {
+        BidderUserID = bidderUserID;
+    }
+
+    public String getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(String amount) {
+        Amount = amount;
+    }
+
+
+
+}
