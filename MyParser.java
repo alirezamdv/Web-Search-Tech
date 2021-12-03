590ef51fb1c3c26d2d04d691e9b4a77395fe6fba
alirezamdv <ali.s3cret@gmail.com>
2021-12-03 13:37:10 +0100

    parsing Items

--- MyParser.java
+++ MyParser.java
@@ -1,10 +1,10 @@
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.text.*;
 import java.util.*;
 
 import org.xml.sax.XMLReader;
 import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 import org.xml.sax.InputSource;
 import org.xml.sax.helpers.XMLReaderFactory;
 import org.xml.sax.helpers.DefaultHandler;
@@ -12,30 +12,42 @@ import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-//import Bid;
 
 public class MyParser extends DefaultHandler {
-//    private Bid bid;
 
-    public static void main(String[] args)
-             {
-                 try {
-                     SAXParserFactory factory = SAXParserFactory.newInstance();
-                     SAXParser saxParser = factory.newSAXParser();
-                     MyParser myParser = new MyParser();
-                     String[] p = {"ebay_data/items-0.xml"};
+    // csv path for tables
+    private static final String ITEM_PATH = "01_Item.csv";
 
-                     // Parse each file provided on the
-                     // command line.
-                     for (String arg : p) {
-                         File inputFile = new File(arg);
+    //for holding  the string value from parsing
+    String temp = "";
 
-                         ((SAXParser) saxParser).parse(inputFile, myParser);
+    boolean inItem = false;
+    boolean inBid = false;
+    boolean isInDescription = false;
+    private Item item;
+    private Bids bids;
 
-                     }
-                 } catch (Exception e) {
-                     e.printStackTrace();
-                 }
+    // for description,
+    private StringBuilder builder;
+
+    public static void main(String[] args) {
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            MyParser myParser = new MyParser();
+            String[] p = {"ebay_data/items-0.xml"};
+
+            // Parse each file provided on the
+            // command line.
+            for (String arg : p) {
+                File inputFile = new File(arg);
+
+                saxParser.parse(inputFile, myParser);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -81,55 +93,164 @@ public class MyParser extends DefaultHandler {
 
 
     public void startElement(String uri, String name,
-                             String qName, Attributes atts) {
+                             String qName, Attributes atts)
+            throws SAXException {
         if (qName.equalsIgnoreCase("Item")) {
-            System.out.println(atts.getValue("ItemID"));
+            inItem = true;
+            item = new Item();
+            bids = new Bids();
+
+
+            // set new items
+            // for holding the itemID
+            String itemID = atts.getValue("ItemID");
+            item.setItemID(itemID);
+            bids.setItemID(itemID);
+
+            System.out.println("in item");
+        } else if (qName.equalsIgnoreCase("Bids")) {
+            inBid = true;
+            System.out.println("in Bid");
+
+        } else if (qName.equalsIgnoreCase("Description")) {
+
+            System.out.println("in Description");
+            isInDescription = true;
+            builder = new StringBuilder();
+
+        } else if (qName.equalsIgnoreCase("Category")) {
+
+            System.out.println("in Category");
 
         }
 
-        if ("".equals (uri))
-            System.out.println("Start element: " + qName);
-        else
-            System.out.println("Start element: {" + uri + "}" + name);
-        for (int i = 0; i < atts.getLength(); i++) {
-            System.out.println("Attribute: " + atts.getLocalName(i) + "=" + atts.getValue(i));
-        }
+
+//        if ("".equals (uri)){
+//        System.out.println("Start element: " + qName);}
+//        else
+//            System.out.println("Start element: {" + uri + "}" + name);
+//        for (int i = 0; i < atts.getLength(); i++) {
+//            System.out.println("Attribute: " + atts.getLocalName(i) + "=" + atts.getValue(i));
+//        }
     }
 
 
     public void endElement(String uri, String name, String qName) {
-        if ("".equals(uri))
-            System.out.println("End element: " + qName);
-        else
-            System.out.println("End element:   {" + uri + "}" + name);
+        if (qName.equalsIgnoreCase("Item")) {
+            writeToTable(item, ITEM_PATH);
+        } else if (qName.equalsIgnoreCase("Name")) {
+            item.setName(temp);
+        } else if (qName.equalsIgnoreCase("Currently")) {
+            item.setCurrently(strip(temp));
+        } else if (qName.equalsIgnoreCase("Buy_Price")) {
+            item.setBuyPrice(strip(temp));
+        } else if (qName.equalsIgnoreCase("First_Bid")) {
+            item.setFirstBid(strip(temp));
+        } else if (qName.equalsIgnoreCase("Number_of_Bids")) {
+            item.setNumberOfBids(temp);
+        } else if (qName.equalsIgnoreCase("Location")) {
+            if (inBid) {
+                //TODO set bidder Location here
+            }
+            item.setLocation(temp);
+        } else if (qName.equalsIgnoreCase("Country")) {
+            if (inBid) {
+                //TODO set bidder country here
+            }
+            item.setCountry(temp);
+
+        } else if (qName.equalsIgnoreCase("Started")) {
+
+            item.setStarted(convertTimeToMySQL(temp));
+
+        } else if (qName.equalsIgnoreCase("Ends")) {
+
+            item.setEnds(convertTimeToMySQL(temp));
+
+        } else if (qName.equalsIgnoreCase("Description")) {
+
+            isInDescription = false;
+
+            String descriptionFullText = builder.toString();
+
+            builder.setLength(0);
+            // shrink the item to 4000 characters only
+            if (descriptionFullText.length() > 4000) {
+                descriptionFullText = descriptionFullText.substring(0, 3999);
+            }
+//             to replace new lines, because this new lines break the number of
+//             rows in db
+            descriptionFullText = descriptionFullText
+                    .replace("\n", "")
+                    .replace("\r", "");
+            item.setDescription(descriptionFullText);
+        }
+
     }
+//        if ("".equals(uri))
+//            System.out.println("End element: " + qName);
+//        else
+//            System.out.println("End element:   {" + uri + "}" + name);
 
 
     public void characters(char ch[], int start, int length) {
-        System.out.print("Characters:    \"");
-        for (int i = start; i < start + length; i++) {
-            switch (ch[i]) {
-                case '\\':
-                    System.out.print("\\\\");
-                    break;
-                case '"':
-                    System.out.print("\\\"");
-                    break;
-                case '\n':
-                    System.out.print("\\n");
-                    break;
-                case '\r':
-                    System.out.print("\\r");
-                    break;
-                case '\t':
-                    System.out.print("\\t");
-                    break;
-                default:
-                    System.out.print(ch[i]);
-                    break;
+
+            if (isInDescription) {
+                builder.append(ch, start, length);
+            }else {
+                // for holding the value from parsing
+                temp = new String(ch, start, length);
             }
+//            switch (ch[i]) {
+//                case '\\':
+//                    System.out.print("\\\\");
+//                    break;
+//                case '"':
+//                    System.out.print("\\\"");
+//                    break;
+//                case '\n':
+//                    System.out.print("\\n");
+//                    break;
+//                case '\r':
+//                    System.out.print("\\r");
+//                    break;
+//                case '\t':
+//                    System.out.print("\\t");
+//                    break;
+//                default:
+//                    System.out.print(ch[i]);
+//                    break;
+//            }
+
+//        System.out.print("\"\n");
+    }
+
+
+    private static <T> void writeToTable(T table, String tablePath) {
+        BufferedWriter bw;
+        try {
+            bw = new BufferedWriter(new FileWriter(tablePath, true));
+            bw.write(table.toString());
+            bw.newLine();
+            bw.flush();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
-        System.out.print("\"\n");
+
+
+    }
+
+    private static String convertTimeToMySQL(String eBayTime) {
+        SimpleDateFormat ebayTimePattern = new SimpleDateFormat("MMM-dd-yy HH:mm:ss");
+        SimpleDateFormat MySQLPattern = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String MySQLFormattedPattern = "";
+        try {
+            Date parsedEbayTime = ebayTimePattern.parse(eBayTime);
+            MySQLFormattedPattern = MySQLPattern.format(parsedEbayTime);
+        } catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return MySQLFormattedPattern;
     }
 
 }
