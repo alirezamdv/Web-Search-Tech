a94376d445e362b13a3c2ecb257e7abd35dbe23e
Herbert <ha_ge@uni-bremen.de>
2021-12-14 14:42:34 +0100

    queries

--- queries.sql
+++ queries.sql
@@ -1,12 +1,10 @@
 -- Find the number of users in the database.
-SELECT count(*) FROM User;
+SELECT COUNT(*) FROM (SELECT User_ID FROM Seller UNION SELECT User_ID FROM Bidder) as x;
 
 -- Find the number of items in "New York", i.e., itmes whose location is exactly the string "New York". Pay special attention to case sensitivity. E.g., you should not match items in "new york".
-
 SELECT * FROM (SELECT * from Item it, Location l  where it.LocationId = l.id and BINARY Location='New York') as il* ;
 
 -- Find the number of auctions belonging to exactly four categories. Be careful to remove duplicates, if you store them.
-
 SELECT count(*) FROM (SELECT ItemID FROM ItemCategory GROUP BY ItemId Having count(Category) >1 ) as ICII;
 
 -- Find the ID(s) of current (unsold) auction(s) with the highest bid. Remember that the data was captured at December 20th, 2001, one second after midnight. Pay special attention to the current auctions without any bid.
@@ -18,10 +16,10 @@ FROM Item
 WHERE Currently = @v1 AND Number_Of_Bids > 0;
 
 -- Find the number of sellers whose rating is higher than 1000.
-SELECT count(*) FROM Item, User WHERE Item.UserID = User.User_ID AND User.Rating > 1000;
+SELECT COUNT(*) FROM Seller WHERE Seller.Rating > 1000;
 
 -- Find the number of users who are both sellers and bidders.
-SELECT count(DISTINCT Item.UserID) FROM Item, Bids WHERE Item.UserID = Bids.UserID;
+SELECT COUNT(*) FROM Seller WHERE Seller.User_ID IN (SELECT User_ID FROM Bidder);
 
 -Find the number of categories that include at least one item with a bid of more than $100.
 
